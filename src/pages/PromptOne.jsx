b01954ba27334cbe4d1f91bf1e7662bb252c5a34
Huan <huan.zeng1@gmail.com>
2022-05-07 01:43:46 -0400

    add another prompt 2

--- src/pages/PromptOne.jsx
+++ src/pages/PromptOne.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {} from '@fortawesome/free-regular-svg-icons'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const PromptOne = () => {
   const [message, setMessage] = useState('Oh no, why?')
+  const [answer, setAnswer] = useState([
+    'Parsonal',
+    'Work',
+    'Life',
+    'Family',
+    'Not Sure'
+  ])
   return (
     <div>
       <div>
-        <h3>{message}</h3>
+        <h1>{message}</h1>
       </div>
-      <div></div>
+      <div>
+        {answer.map((ans, idx) => (
+          <div key={idx}>
+            {ans}
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/PromptTwo.jsx
+++ src/pages/PromptTwo.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const PromptTwo = () => {
+  const [message, setMessage] = useState('Oh no, why?')
+  const [answer, setAnswer] = useState([
+    'Parsonal',
+    'Work',
+    'Life',
+    'Family',
+    'Not Sure'
+  ])
   return <div>PromptTwo</div>
 }
 
