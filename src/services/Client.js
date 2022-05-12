b53c46305b3869f8ba2a2cbd26d15d7aaa3e7fdb
Huan <huan.zeng1@gmail.com>
2022-05-12 12:22:32 -0400

    remove favicon

--- src/services/Client.js
+++ src/services/Client.js
@@ -1,6 +1,9 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL =
+  process.env.Node_ENV === 'production'
+    ? 'https://center-backend.herokuapp.com/'
+    : 'http://localhost:3001'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
